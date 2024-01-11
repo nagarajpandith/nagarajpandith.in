137ae0f347180514693dcb8a8dcb5a4852e971ee
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-01-11 10:26:20 +0530

    fix: prisma adapter typeerror

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/lib/prisma';
 
 export const authOptions: NextAuthOptions = {

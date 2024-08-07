7ea974cb0e3c786a0a8452bfd221b04e5250a34f
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-08-07 08:30:05 +0530

    feat: comment box ui improve

--- components/commentBox.tsx
+++ components/commentBox.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { useSession, signIn } from 'next-auth/react';
 import * as React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { IoLogOutOutline, IoSendSharp } from 'react-icons/io5';
+import Image from 'next/image';
+import { FaGoogle } from 'react-icons/fa';
 
 type CommentFormProps = {
   text: string;
@@ -9,37 +12,84 @@ type CommentFormProps = {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
 };
 
+const CHARACTER_LIMIT = 500;
+
 export default function CommentBox({
   text,
   setText,
   onSubmit,
 }: CommentFormProps) {
   const { data: session } = useSession();
+
   if (session) {
     return (
-      <form onSubmit={onSubmit}>
-        <div className="flex flex-col items-center mt-10">
-          <textarea
-            placeholder="Let us know what you think!"
-            className="px-4 text-black resize"
-            onChange={(e) => setText(() => e.target.value)}
-            value={text}
-          ></textarea>
-          <button className="border mt-6 rounded px-3">Post!</button>
+      <>
+        <div className="mt-5 flex md:flex-row items-center gap-2 text-center">
+          <Image
+            src={session.user?.image!}
+            alt="Profile image"
+            width={25}
+            height={25}
+            className="rounded-full"
+          />
+          <a className="text-sm no-underline">
+            Posting as{' '}
+            <span className="font-semibold">{session?.user?.name}</span>
+          </a>
+          <button
+            className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded"
+            onClick={() => signOut()}
+          >
+            <IoLogOutOutline />
+          </button>
         </div>
-      </form>
+        <div className="my-1">
+          <form className="flex gap-2" onSubmit={onSubmit}>
+            <textarea
+              value={text}
+              onChange={(e) => {
+                if (e.target.value.length <= CHARACTER_LIMIT) {
+                  setText(e.target.value);
+                }
+              }}
+              className="w-full bg-gray-800 rounded-xl focus:outline-none p-1 px-3 resize-none"
+              name="message"
+              placeholder="Post a comment..."
+            />
+            <button
+              className="bg-primary hover:bg-primary/80 text-white font-bold py-2 px-4 rounded-xl"
+              type="submit"
+              disabled={text.length > CHARACTER_LIMIT}
+            >
+              <IoSendSharp />
+            </button>
+          </form>
+          <div className="flex items-center justify-between my-1">
+            <div className="text-right text-sm text-gray-400">
+              {text.length}/{CHARACTER_LIMIT}
+            </div>
+            {text.length === CHARACTER_LIMIT && (
+              <div className="text-red-500 text-sm">
+                Character limit reached!
+              </div>
+            )}
+          </div>
+        </div>
+      </>
     );
   }
   return (
-    <div className="flex flex-col items-center mt-10">
-      <textarea
-        placeholder="Please SignIn to comment"
-        className="px-4 text-black resize"
-        disabled
-      ></textarea>
-      <button className="border mt-6 rounded px-3" onClick={() => signIn()}>
-        SignIn
+    <>
+      <div className="mt-3 mb-3">Hello! Please sign in to comment.</div>
+      <button
+        className="mb-3 bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 rounded"
+        onClick={() => signIn('google')}
+      >
+        <div className="h-5 flex gap-2 items-center">
+          <FaGoogle />
+          Sign In
+        </div>
       </button>
-    </div>
+    </>
   );
 }

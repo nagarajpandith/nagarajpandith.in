9cea6bf502c0464b70c32dbe4db29797662f4154
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-08-24 23:40:25 +0530

    feat: photos lightbox

--- components/designWorkSection.tsx
+++ components/designWorkSection.tsx
@@ -1,57 +1,28 @@
 import { HiChevronDoubleRight } from 'react-icons/hi';
 
 const DesignWorkSection = () => {
+  const designWorkList = [
+    '204036235',
+    '204035799',
+    '204035405',
+    '204035277',
+    '204035119',
+    '204035511',
+  ];
+
   return (
     <>
       <div className="flex flex-wrap gap-5 justify-center lg:justify-between items-center w-full">
-        <iframe
-          src="https://www.behance.net/embed/project/204036235?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <iframe
-          src="https://www.behance.net/embed/project/204035799?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <iframe
-          src="https://www.behance.net/embed/project/204035405?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <iframe
-          src="https://www.behance.net/embed/project/204035277?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <iframe
-          src="https://www.behance.net/embed/project/204035119?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <iframe
-          src="https://www.behance.net/embed/project/204035511?ilo0=1"
-          allowFullScreen
-          className="w-[300px] h-[300px] animate-fade-up"
-          frameBorder="0"
-          allow="clipboard-write"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
+        {designWorkList.map((designWork, i) => (
+          <iframe
+            src={`https://www.behance.net/embed/project/${designWork}?ilo0=1`}
+            allowFullScreen
+            className="w-[300px] h-[300px] animate-fade-up"
+            frameBorder="0"
+            allow="clipboard-write"
+            referrerPolicy="strict-origin-when-cross-origin"
+          ></iframe>
+        ))}
       </div>
       <div className="flex items-center justify-center mt-5">
         <a
--- components/photoWorkSection.tsx
+++ components/photoWorkSection.tsx
@@ -1,7 +1,73 @@
+'use client';
+
+import Image from 'next/image';
+import 'photoswipe/dist/photoswipe.css';
+import { Gallery, Item } from 'react-photoswipe-gallery';
+
 const PhotoWorkSection = () => {
+  const horizontal = Array.from({ length: 6 }, (_, i) => i + 1);
+  const vertical = Array.from({ length: 41 }, (_, i) => i + 1);
+
   return (
-    <div>
-      <>PhotoWorkSection</>
+    <div className="flex justify-center flex-wrap gap-5">
+      <Gallery>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          {vertical.map((index) => (
+            <Item
+              key={index}
+              original={
+                'static/captures/vertical/' + index.toString() + '.jpeg'
+              }
+              thumbnail={
+                'static/captures/vertical/' + index.toString() + '.jpeg'
+              }
+              width="1080"
+              height="1920"
+            >
+              {({ ref, open }) => (
+                <Image
+                  className="animate-fade-up h-auto max-w-full rounded-lg"
+                  width={1080}
+                  height={1920}
+                  alt="image"
+                  ref={ref}
+                  onClick={open}
+                  src={
+                    '/static/captures/vertical/' + index.toString() + '.jpeg'
+                  }
+                />
+              )}
+            </Item>
+          ))}
+        </div>
+        {horizontal.map((index) => (
+          <Item
+            key={index}
+            original={
+              'static/captures/horizontal/' + index.toString() + '.jpeg'
+            }
+            thumbnail={
+              'static/captures/horizontal/' + index.toString() + '.jpeg'
+            }
+            width="960"
+            height="540"
+          >
+            {({ ref, open }) => (
+              <Image
+                className="animate-fade-up rounded-lg"
+                width={960}
+                height={540}
+                alt="image"
+                ref={ref}
+                onClick={open}
+                src={
+                  '/static/captures/horizontal/' + index.toString() + '.jpeg'
+                }
+              />
+            )}
+          </Item>
+        ))}
+      </Gallery>
     </div>
   );
 };

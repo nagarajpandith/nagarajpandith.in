f0962d80da7288fd7fc1c3b156cdebab0b850325
nagarajpandith <nagaraj.pandith2002@gmail.com>
2025-12-20 22:17:00 +0530

    chore: cleanup all dirs & files

--- components/pageTransition.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export const PageTransition = ({
-  children,
-}: {
-  children: ReactNode;
-}): JSX.Element => {
-  return <div className="animate-fade-up">{children}</div>;
-};

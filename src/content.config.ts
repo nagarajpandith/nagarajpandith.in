b42dfa0c775ff31e2689878816d003346c211e5c
nagarajpandith <nagaraj.pandith2002@gmail.com>
2026-03-22 19:41:00 +0530

    fix: lightbox bug, upgrade astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     desc: z.string(),
@@ -14,7 +15,7 @@ const blog = defineCollection({
 });
 
 const notes = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/notes" }),
   schema: z.object({
     title: z.string(),
     type: z.enum(['short', 'long']),
